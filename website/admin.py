38819b14e54ecefc4fd713acfbbace3ed4b53184
Mifthahul Amri <mifthahulamri@gmail.com>
2025-12-31 11:51:25 +0700

    migrasi foto galeri menggunakan django admin

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Berita, Dosen
+from .models import Berita, Dosen, PhotoCollage
 
 admin.site.register(Berita)
 admin.site.register(Dosen)
+admin.site.register(PhotoCollage)
